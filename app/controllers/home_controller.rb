cef8f2fba17f7d0ce7f86da509d47c9d0a948db7
Ayron Little <ayron.little@shopify.com>
2018-01-05 16:09:35 -0500

    Add Printful client calls

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+require  'pp'
+
 class HomeController < ApplicationController
 
   def upload
-    begin
-      cloundinary_image = Cloudinary::Uploader.upload(params[:img])
+    cloundinary_image = img_upload(image_params)
+    if cloundinary_image.present?
       scaled_image_url = cloundinary_image['secure_url'].sub('upload/','upload/c_limit,w_1600/')
       response = {
         url: cloundinary_image['secure_url'],
@@ -13,17 +15,38 @@ class HomeController < ApplicationController
         uploadedImageWidth: cloundinary_image['width']
       }
       render status: 200, json: response
-    rescue
+    else
       render status: 500, json: { message: 'There was a problem loading your image' }
     end
   end
 
   def delete
     begin
-      Cloudinary::Uploader.destroy(params[:publicId])
+      Cloudinary::Uploader.destroy(delete_params)
       render status: 200, json: { message: 'IMAGE DELETED' }
     rescue
       render status: 500, json: { message: 'There was a problem deleting your image' }
+    end
+  end
+
+  def submit
+    cloundinary_image = img_upload(image_params)
+    unless cloundinary_image.present?
+      render status: 500, json: { message: 'There was a problem loading your image' }
+      return
+    end
+
+    printful_client = PrintfulClient.new(ENV['PRINTFUL_API_KEY'])
+
+    begin
+      printful_response = pp printful_client.post('orders', printful_request_body(user_params, cloundinary_image))
+      render status: 200, json: { orderInfo: printful_response }
+    rescue PrintfulApiException => e
+        printful_message = 'There was a Printful API exception: %i %s' % [e.code, e.message]
+        render status: 500, json: { message: printful_message }
+    rescue PrintfulException => e
+        printful_message = 'There was a Printful exception: ' + e.message
+        render status: 500, json: { message: printful_message }
     end
   end
 
@@ -35,5 +58,43 @@ class HomeController < ApplicationController
 
   def delete_params
     params.permit(:publicId)
+  end
+
+  def user_params
+    params.require(:info).permit(:name, :address, :city, :state, :country, :zip, :mugSize)
+  end
+
+  def img_upload(img)
+    begin
+      Cloudinary::Uploader.upload(params[:img])
+    rescue
+      nil
+    end
+  end
+
+  def printful_request_body(user_params, cloundinary_image)
+    mugSizeVariant = if user_params[:mugSize].present? && user_params[:mugSize] == '11oz'
+      1320
+    else
+      4830
+    end
+
+    {
+      "recipient": {
+        "name": user_params[:name],
+        "address1": user_params[:address],
+        "city": user_params[:city],
+        "state_code": user_params[:state],
+        "country_code": user_params[:country],
+        "zip": user_params[:zip]
+      },
+      "items": [{
+        "variant_id": mugSizeVariant,
+        "quantity": 1,
+        "files": [{
+          "url": cloundinary_image['secure_url']
+        }]
+      }]
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'upload', to: 'home#upload'
   post 'delete', to: 'home#delete'
+  post 'submit', to: 'home#submit'
 end
